0e99ced44807da89ba20daf445eab2e474bcb8c0
Petrov Petrovich <152518983+socute727@users.noreply.github.com>
2024-03-30 22:46:05 +0500

    Add files via upload

--- pyfetch.py
+++ pyfetch.py
@@ -7,7 +7,8 @@ memoryTotal = psutil.virtual_memory().total
 memoryTotalr = memoryTotal / 1000000000
 f = print
 distr = distro.name()
-
+system = platform.uname()
+obj_Disk = psutil.disk_usage('/')
 def get_uptime():
     with open('/proc/uptime', 'r') as f:
         uptime_seconds = float(f.readline().split()[0])
@@ -15,7 +16,19 @@ def get_uptime():
 uptimemin = get_uptime() / 60
 uptime = uptimemin / 60
 
-
+#if distr == "Arch Linux":
+#    f("⣿⣧⣿⣿⣿⣿⡟⢸⣿⣿⣿⣿⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣿⣏⣿⣿ ",os.path.expanduser('~'))
+#    f("⣿⢸⣿⡏⣿⣿⣹⢸⣿⣿⣿⣿⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿  --------------------------------------")
+#    f("⣿⢼⡟⣤⣿⣧⣿⣸⣿⣿⣿⣿⣿⣿⣿⢻⣸⣿⢿⣿⣿⣿⣿⣿⣿⡇  OS:", platform.system())
+#    f("⣿⢸⢧⣽⡼⣟⣛⣃⣿⠿⣿⣿⣿⣿⣿⢸⣏⣿⡘⣿⣿⣿⣿⡿⣿⢳  Distro:", distro.name())
+#    f("⣿⡜⣸⡿⠷⠿⢿⣿⡼⡟⣼⡿⣿⣿⡿⡼⣿⣞⣆⡄⢭⢟⣻⡇⡿⣾  Kernel:", platform.release())
+#    f("⡜⣷⢻⣤⣿⡒⠄⠄⠉⣺⣿⣿⣾⣽⣇⣥⡯⠿⠾⣞⣮⣃⢻⣧⣇⣿  Uptime:", round(uptime, 2), "H")
+#    f("⣿⣮⡞⣷⣯⣗⣙⣿⣧⣣⣿⣿⣿⣿⣿⣿⣇⠟⡂⣀⣀⠉⡫⢸⣸⣿  CPU:", get_processor_name           )
+#    f("⢿⣿⣿⣮⡻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣷⣬⣍⣎⣿⣿  CPU usage:", cpuusg, "%")
+#    f("⣦⣭⣟⡿⣿⣿⣝⢿⣿⣿⣿⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣜⣼⣿⣿  RAM:", round(memoryUse, 2),"/", round(memoryTotalr, 2),"GiB")
+#    f("⠋⠄⠄⠄⠄⠉⠻⢷⣝⡿⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⣼⣿⣿⣿  Disc used:", round(obj_Disk.used / 1000000000, 2), "GiB")
+#    f("⠄⠄⠄⠄⠄⠄⠄⠄⢙⢿⣮⡻⣿⣷⣿⣿⠿⣟⡯⢡⣾⣠⣿⣿⣿⣿  Architecture:", platform.machine())
+#    f("")
 if distr == "Arch Linux":
     f("    _             _ ")
     f("   / \\   _ __ ___| |__     ",os.path.expanduser('~'))
